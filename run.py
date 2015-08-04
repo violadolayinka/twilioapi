b9dcd4afc12067bff81144e9cec2fae5e14aa225
violadolayinka <violadolayinka@gmail.com>
2015-08-04 13:39:19 -0700

    Twilio API works. Allows users to call into Twilio number, record a message or be connected to another caller.

--- run.py
+++ run.py
@@ -25,6 +25,11 @@ def hello_monkey():
     # Play an MP3
     resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
 
+    with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+        g.say("""To speak to a real monkey, press 1.
+                 Press 2 to record your own monkey howl.
+                 Press any other key to start over.""")
+
     # Say a command, and listen for the caller to press a key. When they press
     # a key, redirect them to /handle-key.
     with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
@@ -48,9 +53,28 @@ def handle_key():
 
         return str(resp)
 
+    elif digit_pressed == "2":
+        resp = twilio.twiml.Response()
+        resp.say("Record your monkey howl after the tone.")
+        resp.record(maxLength="30", action="/handle-recording")
+        return str(resp)
+
     # If the caller pressed anything but 1, redirect them to the homepage.
     else:
         return redirect("/")
+
+
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+    """Play back the caller's recording."""
+
+    recording_url = request.values.get("RecordingUrl", None)
+
+    resp = twilio.twiml.Response()
+    resp.say("Thanks for howling... take a listen to what you howled.")
+    resp.play(recording_url)
+    resp.say("Goodbye.")
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
